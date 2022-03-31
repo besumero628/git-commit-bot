1e8e423c8e970c6f2047b14b9949269c4388a6c4
NaraSyuhei <syuhei.nara@novel.jpn.com>
2022-04-01 00:36:42 +0900

    Add tweepy tweet

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import twitter
+import tweepy
+import settings
 
-auth = twitter.OAuth(
+# Twitter APIのキーを設定
+consumer_key = settings.TWITTER_CONSUMER_API_KEY
+consumer_secret = settings.TWITTER_CONSUMER_API_SECRET_KEY
 
-)
+# Twitter APIのアクセストークンを設定
+access_token=settings.TWITTER_ACCESS_TOKEN
+access_token_secret =settings.TWITTER_ACCESS_TOKEN_SECRET
+
+# Client 作成
+client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
+
+# 投稿文の設定
+client.create_tweet(text="Hello Twitter!!")
---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# 環境変数のセッティング
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TWITTER_CONSUMER_API_KEY = os.environ.get("TWITTER_CONSUMER_API_KEY")
+TWITTER_CONSUMER_API_SECRET_KEY = os.environ.get("TWITTER_CONSUMER_API_SECRET_KEY")
+TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
